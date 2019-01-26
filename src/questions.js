540742396dce9c2c590b000499b68d8f68c25c7f
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-01-26 21:04:48 -0200

    many improvements

---
+++ src/questions.js
@@ -0,0 +1,146 @@
+const questions = [
+    [
+      'Qual é a única espécie de felino que os exemplares raramente são encontrados sozinhos?',
+      'Qual dos felinos a seguir tem maior o porte?',
+      'Qual dos felinos a seguir costuma caçar à noite?',
+      'Qual dos felinos a seguir tem menor porte?',
+      'O leão macho é o mais lento dentre todos os grandes felinos. Verdadeiro ou falso?',
+      'Qual dos felinos á seguir possui melhor habilidade de escalar árvores?',
+      'Qual dos felinos á seguir tem maior o costume e habilidade para caçar pássaros?',
+      'Quantas raças de gatos domésticos existem aproximadamente?'
+    ],
+    [
+      'Quantos movimentos o planeta Terra executa?',
+      'Qual é a principal consequência do movimento de rotação?',
+      'Qual a principal consequência do movimento de translação?',
+      'Qual a forma real da Terra?',
+      'Quanto tempo a Terra demora para executar o movimento de rotação?',
+      'Quanto tempo a Terra demora para executar o movimento de translação?',
+      'O que é litosfera?',
+      'O que é hidrosfera?',
+      'O que é atmosfera?',
+      'O que é biosfera?'
+    ]
+  ],
+  answers = [
+    [
+      'Leão',
+      'Tigre',
+      'Leão',
+      'Gato do Deserto',
+      'Verdadeiro',
+      'Leoparto',
+      'Caracal',
+      '80'
+    ],
+    [
+      '14 Movimentos',
+      'A formação dos dias e das noites',
+      'A formação das 4 estações do ano',
+      'Geoide',
+      '23 horas, 56 minutos e 4 segundos',
+      '365 dias, 5 horas, 48 minutos e 46 segundos',
+      'É o solo onde pisamos',
+      'É o conjunto de água de todo o planeta',
+      'É a camada gasosa que envolve a Terra',
+      'É o conjunto de sistemas vivos do planeta'
+    ]
+  ],
+  fakeAnswers = [
+    [
+      [
+        'Guepardo',
+        'Tigre',
+        'Gato',
+        'Lince'
+      ],
+      [
+        'Guepardo',
+        'Leopardo',
+        'Caracal',
+        'Onça'
+      ],
+      [
+        'Gato',
+        'Tigre',
+        'Puma',
+        'Leopardo'
+      ],
+      [
+        'Lince',
+        'Caracal',
+        'Gato Pescador'
+      ],
+      [
+        'Falso'
+      ],
+      [
+        'Tigre',
+        'Gato',
+        'Puma'
+      ],
+      [
+        'Lince',
+        'Gato Doméstico'
+      ],
+      [
+        '46',
+        '60',
+        '100'
+      ]
+    ],
+    [
+      [
+        '11 movimentos',
+        '12 movimentos',
+        '13 movimentos',
+        '15 movimentos'
+      ],
+      [
+        'A formação das 4 estações do ano',
+        'A formação das chuvas',
+        'A formação dos fenômenos naturais'
+      ],
+      [
+        'A formação das chuvas',
+        'A manutenção das temperaturas da Terra',
+        'A formação do dia e da noite'
+      ],
+      [
+        'Redonda',
+        'Quadrada',
+        'A Terra não tem forma'
+      ],
+      [
+        '22 horas, 43 minutos e 54 segundos',
+        '23 horas, 56 minutos e 40 segundos',
+        '24 horas, 56 minutos e 4 segundos'
+      ],
+      [
+        '24 horas, 56 minutos e 4 minutos',
+        '364 dias, 5 horas e 45 segundos',
+        '5 horas, 48 minutos e 46 minutos',
+        '365 dias, 1 hora e 46 segundos'
+      ],
+      [
+        'É o conjunto de água de todo o planeta',
+        'É a camada mais extensa da atmosfera',
+        'É o conjunto de sistemas vivos do planeta'
+      ],
+      [
+        'É o solo onde pisamos',
+        'É a camada mais extensa da atmosfera',
+        'É o conjunto de sistemas vivos do planeta'
+      ],
+      [
+        'É o solo onde pisamos',
+        'É o conjunto de água de todo o planeta',
+        'É o conjunto de sistemas vivos do planeta'
+      ],
+      [
+        'É o solo onde pisamos',
+        'É o conjunto de água de todo o planeta',
+        'É a camada gasosa que envolve a Terra'
+      ]
+    ]
+  ]
--- src/script.js
+++ src/script.js
@@ -1,51 +1,14 @@
 const btnNext = document.querySelector('#btnNext'),
   btnStart = document.querySelector('#btnStart'),
   btnGiveUp = document.querySelector('#giveUp'),
+  btnShowAnswers = document.querySelector('#showAnswers'),
   btnFinish = document.querySelector('#finish'),
   selectQuiz = document.querySelector('#selectQuiz'),
   quiz = document.querySelector('#quiz'),
   pagination = document.querySelector('#pagination'),
   winPoints = document.querySelector('#points'),
-  questions = [
-    ['Animais 1', 'Animais 2', 'Animais 3', 'Animais 4', 'Animais 5', 'Animais 6'],
-    ['Países 1', 'Países 2', 'Países 3', 'Países 4', 'Países 5'],
-    ['Pessoas 1', 'Pessoas 2', 'Pessoas 3', 'Pessoas 4', 'Pessoas 5'],
-    ['Comidas 1', 'Comidas 2']
-  ],
-  answers = [
-    ['Resposta Animais 1', 'Resposta Animais 2', 'Resposta Animais 3', 'Resposta Animais 4', 'Resposta Animais 5', 'Resposta Animais 6'],
-    ['Resposta Países 1', 'Resposta Países 2', 'Resposta Países 3', 'Resposta Países 4', 'Resposta Países 5'],
-    ['Resposta Pessoas 1', 'Resposta Pessoas 2', 'Resposta Pessoas 3', 'Resposta Pessoas 4', 'Resposta Pessoas 5'],
-    ['Resposta Comidas 1', 'Resposta Comidas 2']
-  ],
-  fakeAnswers = [
-    [
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3'],
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3'],
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3'],
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3'],
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3'],
-      ['Fake Animais 1', 'Fake Animais 2', 'Fake Animais 3']
-    ],
-    [
-      ['Fake Países 1', 'Fake Países 2', 'Fake Países 3'],
-      ['Fake Países 1', 'Fake Países 2', 'Fake Países 3'],
-      ['Fake Países 1', 'Fake Países 2', 'Fake Países 3'],
-      ['Fake Países 1', 'Fake Países 2', 'Fake Países 3'],
-      ['Fake Países 1', 'Fake Países 2', 'Fake Países 3']
-    ],
-    [
-      ['Fake Pessoas 1', 'Fake Pessoas 2', 'Fake Pessoas 3'],
-      ['Fake Pessoas 1', 'Fake Pessoas 2', 'Fake Pessoas 3'],
-      ['Fake Pessoas 1', 'Fake Pessoas 2', 'Fake Pessoas 3'],
-      ['Fake Pessoas 1', 'Fake Pessoas 2', 'Fake Pessoas 3'],
-      ['Fake Pessoas 1', 'Fake Pessoas 2', 'Fake Pessoas 3']
-    ],
-    [
-      ['Fake Comidas 1', 'Fake Comidas 2', 'Fake Comidas 3'],
-      ['Fake Comidas 1', 'Fake Comidas 2', 'Fake Comidas 3']
-    ]
-  ]
+  star = document.querySelector('#star'),
+  showAnswers = document.querySelector('#answers')
 
 let points = 0,
   currentQuestion = 0,
@@ -55,18 +18,18 @@ let points = 0,
 function render() {
   let html = `<h6>${currentQuestion + 1}° ${questions[selectedType][currentQuestion]}</h6>`
 
-  const answersQuestion = [answers[selectedType][currentQuestion], fakeAnswers[selectedType][currentQuestion][0], fakeAnswers[selectedType][currentQuestion][1], fakeAnswers[selectedType][currentQuestion][2]],
+  const answersQuestion = [answers[selectedType][currentQuestion], ...fakeAnswers[selectedType][currentQuestion]],
     answersRandom = []
 
   for (let i = 0; i < answersQuestion.length; i++) {
-    let rndNumber = parseInt(Math.random() * 4)
+    let rndNumber = parseInt(Math.random() * answersQuestion.length)
 
     while (answersRandom.indexOf(rndNumber) !== -1)
       rndNumber = parseInt(Math.random() * answersQuestion.length)
 
     answersRandom[i] = rndNumber
 
-    const alphabet = ['A', 'B', 'C', 'D']
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
     html += `
         <p>
           <label>
@@ -81,11 +44,22 @@ function render() {
   quiz.innerHTML = html
 }
 
+function createPagination() {
+  htmlPagination = '<li class="disabled"><a><i class="material-icons">chevron_left</i></a></li>'
+
+  for (let i = 0; i < questions[selectedType].length; i++)
+    htmlPagination += `<li class="${currentQuestion === i ? 'active teal c-df no-border waves-effect waves-light' : `${matches[i] === undefined ? 'disabled' : matches[i] === 1 ? `active green c-df no-border ${i === questions[selectedType].length - 1 ? '' : 'brw'}` : `active red c-df no-border ${i === questions[selectedType].length - 1 ? '' : 'brw'}`}`}"><a>${i + 1}</a></li>`
+
+  htmlPagination += '<li class="disabled"><a><i class="material-icons">chevron_right</i></a></li>'
+
+  pagination.innerHTML = htmlPagination
+}
+
 function start() {
   selectQuiz.style.display = 'none'
   btnStart.style.display = 'none'
-  quiz.className = 'card-panel hoverable'
-  btnNext.className = 'btn waves-effect waves-light'
+  quiz.className = 'card-panel'
+  btnNext.className = 'btn waves-effect waves-light scale-transition scale-in'
   btnGiveUp.className = 'btn right waves-effect waves-light modal-trigger red'
   pagination.className = 'pagination center'
 
@@ -100,8 +74,9 @@ function stop() {
   selectQuiz.style.display = 'block'
   btnStart.style.display = 'block'
   quiz.className = 'hide'
-  btnNext.className = 'hide'
+  btnNext.className = 'btn waves-effect waves-light scale-transition scale-out'
   btnGiveUp.className = 'hide'
+  btnShowAnswers.className = 'hide'
   btnFinish.className = 'hide'
   pagination.className = 'hide'
 
@@ -140,24 +115,23 @@ function next() {
       currentAnswers.forEach(answer => { answer.disabled = true })
       btnNext.className = 'btn waves-effect waves-light scale-transition scale-out'
       btnGiveUp.className = 'hide'
-      btnFinish.className = 'btn right waves-effect waves-light scale-transition green scale-in'
+      btnShowAnswers.className = 'btn waves-effect waves-light right'
+      btnShowAnswers.onclick = () => {
+        let html = '<h4><i class="material-icons" style="top:2px">question_answer</i> Respostas</h4>'
+        for (let i = 0; i < answers[selectedType].length; i++)
+          html += `<p>${i + 1}. ${answers[selectedType][i]} <i class="material-icons icons ${matches[i] === 0 ? 'red-text' : 'green-text'}">${matches[i] === 0 ? 'clear' : 'done'}</i></p>`
+
+        showAnswers.innerHTML = html
+        M.Modal.getInstance(document.querySelector('#modal3')).open()
+      }
+      btnFinish.className = 'btn right waves-effect waves-light green'
       const pointsPercentage = points / answers[selectedType].length * 100
-      winPoints.innerHTML = `${points} de ${answers[selectedType].length} e obteve um desempenho de <span class="${pointsPercentage < 50 ? 'red-text' : 'green-text'}">${pointsPercentage}%</span>`
+      winPoints.innerHTML = `${points} de ${answers[selectedType].length} e obteve um desempenho de <span class="${pointsPercentage < 50 ? 'red-text' : 'green-text'}">${pointsPercentage.toFixed(1)}%</span>`
+      star.className = `material-icons ${pointsPercentage < 50 ? 'red-text' : 'green-text'}`
       M.Modal.getInstance(document.querySelector('#modal2')).open()
       createPagination()
     } else render()
   }
-}
-
-function createPagination() {
-  htmlPagination = '<li class="disabled"><a><i class="material-icons">chevron_left</i></a></li>'
-
-  for (let i = 0; i < questions[selectedType].length; i++)
-    htmlPagination += `<li class="${currentQuestion === i ? 'active teal c-df no-border' : `${matches[i] === undefined ? 'disabled' : matches[i] === 1 ? `active green c-df no-border ${i === questions[selectedType].length - 1 ? '' : 'brw'}` : `active red c-df no-border ${i === questions[selectedType].length - 1 ? '' : 'brw'}`}`}"><a>${i + 1}</a></li>`
-
-  htmlPagination += '<li class="disabled"><a><i class="material-icons">chevron_right</i></a></li>'
-
-  pagination.innerHTML = htmlPagination
 }
 
 btnStart.onclick = start
