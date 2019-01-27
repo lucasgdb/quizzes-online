b1c103b6e7f03456092960b570a2e341ab8efd6b
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-01-26 23:18:25 -0200

    changes on about path

---
+++ sobre/src/script.js
@@ -0,0 +1,1 @@
+M.Sidenav.init(document.querySelectorAll('.sidenav')),M.Materialbox.init(document.querySelectorAll('.materialboxed'));if (innerWidth >= 1024)document.querySelector('#card').className = 'card horizontal'
